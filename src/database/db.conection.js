8c9c0984178039f975d5e5541e82dad3e2d5c1f5
noeliajudithzelaya <noeliajudithzelayautnfrt@gmail.com>
2024-09-11 21:36:56 -0300

    se realizo la conexión a la bd

--- src/database/db.conection.js
+++ src/database/db.conection.js
@@ -1,5 +1,5 @@
 import { connect } from "mongoose";
-import { MONGODB_URI } from '../config'
+import { MONGODB_URI }  from '../config'
 
 connect(MONGODB_URI).then(resp => console.log(`db conectada en ${resp.connection.name}`)).catch((error => console.log(error)))
     
